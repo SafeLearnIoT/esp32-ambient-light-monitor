e243658dfb963b03ad03553b7c9d59a58e8d9c71
Mikołaj Macura <mikolajmacura0510@gmail.com>
2024-08-15 21:29:34 +0200

    Remove SD manager + Update to new ML mng

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,7 @@
 
 #include "env.h"
 #include "communication.h"
-#include "rtpnn.h"
-#include "data.h"
+#include "ml.h"
 
 #define LIGHT_SENSOR_PIN A1
 unsigned long lastDataUploadMillis = 0;
@@ -18,7 +17,7 @@ void callback(String &topic, String &payload)
 
 unsigned long lastDataSaveMillis = 0;
 
-rTPNN::SDP<uint16_t> analog_read_sdp(rTPNN::SDPType::Light);
+ML *analog_read_ml;
 
 auto comm = Communication::get_instance(SSID_ENV, PASSWORD_ENV, "esp32/light/", MQTT_HOST_ENV, MQTT_PORT_ENV, callback);
 
@@ -33,34 +32,51 @@ void setup()
   delay(5000);
 
   comm->pause_communication();
+
+  switch (comm->get_ml_algo())
+  {
+  case MLAlgo::LinReg:
+    analog_read_ml = new Regression::Linear(SensorType::Light);
+    break;
+  case MLAlgo::LogReg:
+    analog_read_ml = new Regression::Logistic(SensorType::Light);
+    break;
+  case MLAlgo::rTPNN:
+    analog_read_ml = new RTPNN::SDP(SensorType::Light);
+    break;
+  case MLAlgo::None:
+    break;
+  }
 }
 
 void loop()
 {
-  if (millis() - lastDataSaveMillis > 60000) // 600000) // 60000 - minute
+  if (millis() - lastDataSaveMillis > 60000) // 60000 - minute
   {
     lastDataSaveMillis = millis();
     auto analog_read = analogRead(LIGHT_SENSOR_PIN);
+    auto raw_time = comm->get_rawtime();
+    auto time_struct = localtime(&raw_time);
 
     JsonDocument sensor_data;
-    sensor_data["time"] = comm->get_rawtime();
+    sensor_data["time"] = raw_time;
     sensor_data["device"] = comm->get_client_id();
     JsonObject detail_sensor_data = sensor_data["data"].to<JsonObject>();
     detail_sensor_data["analog_read"] = analog_read;
 
-    JsonDocument rtpnn_data;
-    rtpnn_data["time"] = comm->get_rawtime();
-    rtpnn_data["device"] = comm->get_client_id();
-    rtpnn_data["ml_algo"] = "rtpnn";
-    JsonObject detail_rtpnn_data = rtpnn_data["data"].to<JsonObject>();
+    JsonDocument ml_data;
 
-    JsonObject analog_read_data = detail_rtpnn_data["analog_read"].to<JsonObject>();
-    auto analog_read_calc = analog_read_sdp.execute_sdp(analog_read);
-    analog_read_data["trend"] = analog_read_calc.first;
-    analog_read_data["level"] = analog_read_calc.second;
+    if (comm->get_ml_algo() != MLAlgo::None)
+    {
+      ml_data["time"] = raw_time;
+      ml_data["device"] = comm->get_client_id();
+      ml_data["ml_algo"] = "rtpnn";
+      JsonObject detail_data = ml_data["data"].to<JsonObject>();
 
-    JsonDocument reglin_data;
+      JsonObject analog_read_data = detail_data["analog_read"].to<JsonObject>();
+      analog_read_data = analog_read_ml->perform(*time_struct, analog_read);
+    }
 
-    comm->send_data(sensor_data, rtpnn_data, reglin_data);
+    comm->send_data(sensor_data, ml_data);
   }
 }
