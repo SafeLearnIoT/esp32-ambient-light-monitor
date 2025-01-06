bbe192731bdb726cb41850e839a970a13034616c
Mikołaj Macura <mikolajmacura0510@gmail.com>
2025-01-06 18:52:26 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,10 @@ void callback(String &topic, String &payload)
 
 unsigned long lastDataSaveMillis = 0;
 
-ML *analog_read_ml;
+auto ml_algo = MLAlgo::None;
+auto analog_read_ml = ML(Light, ml_algo, "analog_read");
 
-auto comm = Communication::get_instance(SSID_ENV, PASSWORD_ENV, "esp32/light/", MQTT_HOST_ENV, MQTT_PORT_ENV, callback);
+auto comm = Communication::get_instance(SSID_ENV, PASSWORD_ENV, "esp32/light", MQTT_HOST_ENV, MQTT_PORT_ENV, callback);
 
 void setup()
 {
@@ -32,49 +33,34 @@ void setup()
   delay(5000);
 
   comm->pause_communication();
-
-  switch (comm->get_ml_algo())
-  {
-  case MLAlgo::LinReg:
-    analog_read_ml = new Regression::Linear(SensorType::Light);
-    break;
-  case MLAlgo::LogReg:
-    analog_read_ml = new Regression::Logistic(SensorType::Light);
-    break;
-  case MLAlgo::rTPNN:
-    analog_read_ml = new RTPNN::SDP(SensorType::Light);
-    break;
-  case MLAlgo::None:
-    break;
-  }
 }
 
 void loop()
 {
-  if (millis() - lastDataSaveMillis > 60000) // 60000 - minute
+  if (millis() - lastDataSaveMillis > 900000) // 60000 - minute
   {
+    comm->resume_communication();
     lastDataSaveMillis = millis();
     auto analog_read = analogRead(LIGHT_SENSOR_PIN);
     auto raw_time = comm->get_rawtime();
     auto time_struct = localtime(&raw_time);
 
     JsonDocument sensor_data;
-    sensor_data["time"] = raw_time;
+    sensor_data["time_sent"] = raw_time;
     sensor_data["device"] = comm->get_client_id();
     JsonObject detail_sensor_data = sensor_data["data"].to<JsonObject>();
     detail_sensor_data["analog_read"] = analog_read;
 
     JsonDocument ml_data;
 
-    if (comm->get_ml_algo() != MLAlgo::None)
+    if (ml_algo != MLAlgo::None)
     {
-      ml_data["time"] = raw_time;
+      ml_data["time_sent"] = raw_time;
       ml_data["device"] = comm->get_client_id();
-      ml_data["ml_algo"] = "rtpnn";
-      JsonObject detail_data = ml_data["data"].to<JsonObject>();
+      ml_data["ml_algo"] = algoString[ml_algo];
+      JsonObject data = ml_data["data"].to<JsonObject>();
 
-      JsonObject analog_read_data = detail_data["analog_read"].to<JsonObject>();
-      analog_read_data = analog_read_ml->perform(*time_struct, analog_read);
+      analog_read_ml.perform(*time_struct, analog_read, data);
     }
 
     comm->send_data(sensor_data, ml_data);
