7dacb69efbdcda8f1c3179c16692e1f592f883db
mikomac405 <mikolajmacura0510@gmail.com>
2025-01-12 22:02:57 +0100

    Update library paths and enhance MQTT communication handling

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,31 @@
 #include "ml.h"
 
 #define LIGHT_SENSOR_PIN A1
-unsigned long lastDataUploadMillis = 0;
-void callback(String &topic, String &payload)
-{
-  Serial.print("Topic: ");
-  Serial.print(topic);
-  Serial.print(", Payload: ");
-  Serial.println(payload);
-}
+
+auto analog_read_ml = ML(Light, "analog_read");
 
 unsigned long lastDataSaveMillis = 0;
 
-auto ml_algo = MLAlgo::None;
-auto analog_read_ml = ML(Light, ml_algo, "analog_read");
+void callback(String &topic, String &payload);
 
 auto comm = Communication::get_instance(SSID_ENV, PASSWORD_ENV, "esp32/light", MQTT_HOST_ENV, MQTT_PORT_ENV, callback);
+
+void callback(String &topic, String &payload)
+{
+    Serial.println("[SUB][" + topic + "] " + payload);
+    if(payload == "get_weights")
+    {
+        comm->hold_connection();
+        comm->publish("cmd", "", true);
+        auto weights = analog_read_ml.get_weights();
+        serializeJson(weights, Serial);
+        Serial.println();
+    }
+    if(payload=="set_weights")
+    {
+        comm->release_connection();
+    }
+}
 
 void setup()
 {
@@ -29,21 +39,17 @@ void setup()
   pinMode(LIGHT_SENSOR_PIN, INPUT);
 
   comm->setup();
-
-  delay(5000);
-
-  comm->pause_communication();
 }
 
 void loop()
 {
+  comm->handle_mqtt_loop();
   if (millis() - lastDataSaveMillis > 900000) // 60000 - minute
   {
     comm->resume_communication();
     lastDataSaveMillis = millis();
     auto analog_read = analogRead(LIGHT_SENSOR_PIN);
     auto raw_time = comm->get_rawtime();
-    auto time_struct = localtime(&raw_time);
 
     JsonDocument sensor_data;
     sensor_data["time_sent"] = raw_time;
@@ -51,18 +57,10 @@ void loop()
     JsonObject detail_sensor_data = sensor_data["data"].to<JsonObject>();
     detail_sensor_data["analog_read"] = analog_read;
 
-    JsonDocument ml_data;
 
-    if (ml_algo != MLAlgo::None)
-    {
-      ml_data["time_sent"] = raw_time;
-      ml_data["device"] = comm->get_client_id();
-      ml_data["ml_algo"] = algoString[ml_algo];
-      JsonObject data = ml_data["data"].to<JsonObject>();
-
-      analog_read_ml.perform(*time_struct, analog_read, data);
-    }
-
-    comm->send_data(sensor_data, ml_data);
+    analog_read_ml.perform(analog_read);
+    
+    serializeJson(sensor_data, Serial);
+    comm->send_data(sensor_data);
   }
 }
