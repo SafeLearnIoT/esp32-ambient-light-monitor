cbe3182a20a7f748312f82f98c27b2b82c6fffde
mikomac405 <mikolajmacura0510@gmail.com>
2025-03-01 14:52:01 +0100

    Enhance configuration handling and update build flags for C++17 support

--- src/main.cpp
+++ src/main.cpp
@@ -22,20 +22,29 @@ String payload_content;
 
 void callback(String &topic, String &payload)
 {
-    Serial.println("[SUB][" + topic + "] " + payload);
-    if(payload == "get_weights")
+  if (topic == "configuration")
+  {
+    comm->initConfig(payload);
+    return;
+  }
+  Serial.println("[SUB][" + topic + "] " + payload);
+  if (payload == "get_weights")
+  {
+    if (!get_weights)
     {
-        if(!get_weights){
-            get_weights = true;
-        }
+      get_weights = true;
     }
-    if(payload.startsWith("set_weights;"))
+    return;
+  }
+  if (payload.startsWith("set_weights;"))
+  {
+    if (!set_weights)
     {
-       if(!set_weights){
-           payload_content = payload;
-           set_weights = true;
-       }
+      payload_content = payload;
+      set_weights = true;
     }
+    return;
+  }
 }
 
 void setup()
@@ -51,67 +60,79 @@ void setup()
 
 void loop()
 {
-  if(get_weights)
+  if (comm->is_system_configured())
+  {
+    if (get_weights)
     {
-        comm->hold_connection();
+      comm->hold_connection();
 
-        JsonDocument doc;
-        analog_read_ml.get_weights(doc);
-        comm->publish("cmd_mcu", "weights;" + doc.as<String>());
-        get_weights = false;
+      JsonDocument doc;
+      analog_read_ml.get_weights(doc);
+      comm->publish("cmd_mcu", "weights;" + doc.as<String>());
+      get_weights = false;
     }
-    if(set_weights)
-    {   
-        JsonDocument doc;
-        if(payload_content.substring(12)=="ok"){
-            comm->publish("cmd_gateway", "", true);
-            comm->release_connection();
-            payload_content.clear();
-            set_weights = false;
-            return;
-        }
-        deserializeJson(doc, payload_content.substring(12));
-        
-        auto analog_read_weights = Converter<std::array<double, 4>>::fromJson(doc["analog_read"]);
-        Serial.print("[Analog read]");
-        analog_read_ml.set_weights(analog_read_weights);
-
-        // Clear message
+    if (set_weights)
+    {
+      JsonDocument doc;
+      if (payload_content.substring(12) == "ok")
+      {
         comm->publish("cmd_gateway", "", true);
-
         comm->release_connection();
         payload_content.clear();
         set_weights = false;
+        return;
+      }
+      deserializeJson(doc, payload_content.substring(12));
+
+      auto analog_read_weights = Converter<std::array<double, 4>>::fromJson(doc["analog_read"]);
+      Serial.print("[Analog read]");
+      analog_read_ml.set_weights(analog_read_weights);
+
+      // Clear message
+      comm->publish("cmd_gateway", "", true);
+
+      comm->release_connection();
+      payload_content.clear();
+      set_weights = false;
     }
 
-  comm->handle_mqtt_loop();
-  if (millis() - lastDataSaveMillis > 900000) // 60000 - minute
-  {
-    comm->resume_communication();
-    lastDataSaveMillis = millis();
-    auto analog_read = analogRead(LIGHT_SENSOR_PIN);
-    auto raw_time = comm->get_rawtime();
-
-    JsonDocument sensor_data;
-    sensor_data["time_sent"] = raw_time;
-    sensor_data["device"] = comm->get_client_id();
-    JsonObject detail_sensor_data = sensor_data["data"].to<JsonObject>();
-    detail_sensor_data["analog_read"] = analog_read;
-
-    JsonDocument ml_data;
-    ml_data["time_sent"] = raw_time;
-    ml_data["device"] = comm->get_client_id();
-    JsonObject detail_ml_data = ml_data["data"].to<JsonObject>();
-
-    JsonObject analog_read_data = detail_ml_data["analog_read"].to<JsonObject>();
-    analog_read_ml.perform(analog_read, analog_read_data);
+    comm->handle_mqtt_loop();
     
-    //serializeJson(sensor_data, Serial);
-    //Serial.println();
-    //comm->send_data(sensor_data);
+    if (millis() - lastDataSaveMillis > 900000) // 60000 - minute
+    {
+      comm->resume_communication();
+      lastDataSaveMillis = millis();
+      auto analog_read = analogRead(LIGHT_SENSOR_PIN);
+      auto raw_time = comm->get_rawtime();
 
-    serializeJson(ml_data, Serial);
-    Serial.println();
-    comm->send_ml(ml_data);
+      if (comm->m_configuration->getSendSensorData())
+      {
+        JsonDocument sensor_data;
+        sensor_data["time_sent"] = raw_time;
+        sensor_data["device"] = comm->get_client_id();
+        JsonObject detail_sensor_data = sensor_data["data"].to<JsonObject>();
+        detail_sensor_data["analog_read"] = analog_read;
+
+        comm->send_data(sensor_data);
+      }
+
+      if (comm->m_configuration->getRunMachineLearning())
+      {
+        JsonDocument ml_data;
+        ml_data["time_sent"] = raw_time;
+        ml_data["device"] = comm->get_client_id();
+        JsonObject detail_ml_data = ml_data["data"].to<JsonObject>();
+
+        JsonObject analog_read_data = detail_ml_data["analog_read"].to<JsonObject>();
+        analog_read_ml.perform(analog_read, analog_read_data);
+        comm->send_ml(ml_data);
+      }
+    }
+  }
+  else
+  {
+    Serial.println("System not configured");
+    comm->handle_mqtt_loop();
+    delay(1000);
   }
 }
