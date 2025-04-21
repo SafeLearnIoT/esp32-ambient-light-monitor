1a9d626c3a01e3b575ac3f4e6fa3ed4efd7bd183
mikomac405 <mikolajmacura0510@gmail.com>
2025-04-21 20:14:15 +0200

    Refactor parameter handling in MQTT communication and enhance data processing logic

--- src/main.cpp
+++ src/main.cpp
@@ -8,43 +8,42 @@
 
 #define LIGHT_SENSOR_PIN A1
 
-auto analog_read_ml = ML(Light, "analog_read");
-
 unsigned long lastDataSaveMillis = 0;
 
 void callback(String &topic, String &payload);
-
 auto comm = Communication::get_instance(SSID_ENV, PASSWORD_ENV, "esp32/light", MQTT_HOST_ENV, MQTT_PORT_ENV, callback);
 
-bool get_weights = false;
-bool set_weights = false;
+auto analog_read_ml = ML(Light, "analog_read");
+
+bool get_params = false;
+bool set_params = false;
 String payload_content;
 
 void callback(String &topic, String &payload)
 {
-  if (topic == "configuration")
-  {
-    comm->initConfig(payload);
-    return;
-  }
-  Serial.println("[SUB][" + topic + "] " + payload);
-  if (payload == "get_weights")
-  {
-    if (!get_weights)
+    if (topic == "configuration" && !comm->is_system_configured())
     {
-      get_weights = true;
+        comm->initConfig(payload);
+        return;
     }
-    return;
-  }
-  if (payload.startsWith("set_weights;"))
-  {
-    if (!set_weights)
+    Serial.println("[SUB][" + topic + "] " + payload);
+    if (payload == "get_params")
     {
-      payload_content = payload;
-      set_weights = true;
+        if (!get_params)
+        {
+            get_params = true;
+        }
+        return;
     }
-    return;
-  }
+    if (payload.startsWith("set_params;"))
+    {
+        if (!set_params)
+        {
+            payload_content = payload;
+            set_params = true;
+        }
+        return;
+    }
 }
 
 void setup()
@@ -62,43 +61,52 @@ void loop()
 {
   if (comm->is_system_configured())
   {
-    if (get_weights)
+    if (get_params)
     {
       comm->hold_connection();
 
       JsonDocument doc;
-      analog_read_ml.get_weights(doc);
-      comm->publish("cmd_mcu", "weights;" + doc.as<String>());
-      get_weights = false;
+      auto raw_time = comm->get_rawtime();
+
+      doc["time_sent"] = raw_time;
+      doc["device"] = comm->get_client_id();
+
+      JsonObject data = doc["data"].to<JsonObject>();
+      
+
+      data["type"] = "params";
+      analog_read_ml.get_params(data);
+      comm->publish("cmd_mcu", "params;" + doc.as<String>());
+      get_params = false;
     }
-    if (set_weights)
+    if (set_params)
     {
       JsonDocument doc;
-      if (payload_content.substring(12) == "ok")
+      if (payload_content.substring(11) == "ok")
       {
         comm->publish("cmd_gateway", "", true);
         comm->release_connection();
         payload_content.clear();
-        set_weights = false;
+        set_params = false;
         return;
       }
-      deserializeJson(doc, payload_content.substring(12));
+      deserializeJson(doc, payload_content.substring(11));
 
-      auto analog_read_weights = Converter<std::array<double, 4>>::fromJson(doc["analog_read"]);
+      auto analog_read_weights = Converter<std::array<double, 8>>::fromJson(doc["analog_read"]);
       Serial.print("[Analog read]");
-      analog_read_ml.set_weights(analog_read_weights);
+      analog_read_ml.set_params(analog_read_weights);
 
       // Clear message
       comm->publish("cmd_gateway", "", true);
 
       comm->release_connection();
       payload_content.clear();
-      set_weights = false;
+      set_params = false;
     }
 
     comm->handle_mqtt_loop();
     
-    if (millis() - lastDataSaveMillis > 900000) // 60000 - minute
+    if (millis() - lastDataSaveMillis > comm->m_configuration->getActionIntervalMillis()) // 60000 - minute
     {
       comm->resume_communication();
       lastDataSaveMillis = millis();
@@ -111,6 +119,7 @@ void loop()
         sensor_data["time_sent"] = raw_time;
         sensor_data["device"] = comm->get_client_id();
         JsonObject detail_sensor_data = sensor_data["data"].to<JsonObject>();
+        detail_sensor_data["test_name"] = comm->m_configuration->getTestName();
         detail_sensor_data["analog_read"] = analog_read;
 
         comm->send_data(sensor_data);
@@ -122,9 +131,13 @@ void loop()
         ml_data["time_sent"] = raw_time;
         ml_data["device"] = comm->get_client_id();
         JsonObject detail_ml_data = ml_data["data"].to<JsonObject>();
+        detail_ml_data["type"] = "prediction";
+        detail_ml_data["test_name"] = comm->m_configuration->getTestName();
 
         JsonObject analog_read_data = detail_ml_data["analog_read"].to<JsonObject>();
-        analog_read_ml.perform(analog_read, analog_read_data);
+
+        auto training_mode = comm->m_configuration->getMachineLearningTrainingMode();
+        analog_read_ml.perform(analog_read, analog_read_data, training_mode);
         comm->send_ml(ml_data);
       }
     }
